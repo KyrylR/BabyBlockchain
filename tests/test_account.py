392a87142bc2aace7b20524730b07cb7ebe6728c
Kyrylo Riabov <kyryl.ryabov@gmail.com>
2022-07-16 16:27:45 +0300

    Added first test for account class to check its generation

--- tests/test_account.py
+++ tests/test_account.py
@@ -1,5 +1,15 @@
+import unittest
+
 from blockchain.account import Account
 
 
-if __name__ == "__main__":
-    acc = Account()
+class AccountTestCase(unittest.TestCase):
+    def test_unique_accounts(self):
+        account_gen = Account()
+        first = account_gen.get_account()
+        second = account_gen.get_account()
+        self.assertNotEqual(first, second)
+
+
+if __name__ == '__main__':
+    unittest.main()
