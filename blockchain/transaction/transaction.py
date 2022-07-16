ede767f22e0b877168609973fe188480e6b0d46a
Kyrylo Riabov <kyryl.ryabov@gmail.com>
2022-07-16 19:19:03 +0300

    Fix bugs in Transaction class

--- blockchain/transaction/transaction.py
+++ blockchain/transaction/transaction.py
@@ -4,13 +4,15 @@
 была уже добавлена в историю. Для этого можно использовать значение nonce, которое влияет на формирование идентификатора
 транзакции (хеш-значение от всех данных транзакции).
 """
+from copy import deepcopy
 from dataclasses import dataclass, field
 from typing import Optional, List
 
-from hash_lib.Keccak import Keccak
+from hash_lib.SHA1 import SHA1
 
 from blockchain.account import Account
 from blockchain.transaction.operation import Operation
+from features.utils import performance
 
 
 @dataclass
@@ -25,42 +27,44 @@ class Transaction:
     # in range [0, 255]
     sequence: int = field(default=0)
 
-    def __post_init__(self):
-        print(self.__repr__())
-        self.transaction_id = Keccak().update(self.__repr__().encode())
+    # @performance
+    def __initialize_fields(self, operations: List[Operation], sequence: int):
+        self.set_of_operations = operations
+        self.sequence = sequence
+        self.transaction_id = SHA1().update(self.__repr__().encode())
 
-    @staticmethod
-    def crete_transaction(operations: List[Operation], sequence: int) -> "Transaction":
+    def crete_transaction(self, operations: List[Operation], sequence: int) -> Optional["Transaction"]:
         """
         The function allows to create a transaction with all necessary details.
         It takes a list of transactions and nonce as input.
         :return: Transaction object.
         """
-        if sequence < 0:
-            sequence = 0
-        elif sequence > 255:
-            sequence = 255
+        self.__initialize_fields(operations, sequence)
+        if self.verify_transaction():
+            return deepcopy(self)
+        return None
 
-        return Transaction(set_of_operations=operations, sequence=sequence)
-
-    @staticmethod
-    def crete_coinbase_transaction(miner: Account, amount: int) -> "Transaction":
+    def crete_coinbase_transaction(self, miner: Account, amount: int) -> Optional["Transaction"]:
         """
         The function allows to create a transaction with all necessary details.
         It takes a list of transactions and nonce as input.
         :return: Transaction object.
         """
         op = Operation().create_coinbase_op(miner, amount)
-        return Transaction(set_of_operations=[op], sequence=255)
+        self.__initialize_fields([op], 255)
+        if self.verify_transaction(True):
+            return deepcopy(self)
+        return None
 
-    def verify_transaction(self) -> bool:
+    def verify_transaction(self, coinbase: bool = False) -> bool:
         if self.sequence < 0 or \
                 self.sequence > 255 or \
-                self.transaction_id != Keccak().update(self.__repr__().encode()):
+                self.transaction_id is None or \
+                self.transaction_id != SHA1().update(self.__repr__().encode()):
             return False
 
         for op in self.set_of_operations:
-            if not op.verify_operation():
+            if op is None or (not op.verify_operation() and not coinbase):
                 return False
 
         return True
